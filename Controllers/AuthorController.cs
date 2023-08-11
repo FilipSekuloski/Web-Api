8d3f8cca1d4214fd4907fd0a27e1a1555070157f
FilipSekuloski <65288579+FilipSekuloski@users.noreply.github.com>
2023-08-11 20:20:03 +0200

    Add files via upload

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vezbi.Data;
+using Vezbi.Dtos;
+using Vezbi.Entities;
+
+namespace Vezbi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private BooksDbContext dbContext;
+
+        public AuthorController(BooksDbContext dbContext) 
+        {
+            this.dbContext=dbContext;//container injector
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var result= dbContext.Authors.Select(x=> new AuthorDto
+            {
+                Id= x.Id,
+                Name= x.Name,
+                DateOfBirth = x.BirthYear
+            }).ToList();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthorById(int id)
+        {
+            var author=dbContext.Authors.AsNoTracking().FirstOrDefault(x=>x.Id==id);
+            if(author==null)
+            {
+                return NotFound();  
+            }
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthor(CreateAuthorDto dto)
+        {
+            var author = new Author
+            {
+                BirthYear = dto.DateOfBirth,
+                Name = dto.Name
+            };
+            dbContext.Authors.Add(author);
+            dbContext.SaveChanges();
+            return Ok(author);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAuthor(int id, Author author)
+        {
+            var toUpdate= dbContext.Authors.FirstOrDefault(x=>x.Id==id);
+            if(toUpdate==null)
+            {
+                return NotFound();
+            }
+            toUpdate.Name= author.Name;
+            toUpdate.BirthYear= author.BirthYear;
+            dbContext.SaveChanges();
+            return Ok(author);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            var author=dbContext.Authors.FirstOrDefault(x=>x.Id== id);
+            if(author==null)
+            {
+                return NotFound();
+            }
+            dbContext.Authors.Remove(author);
+            dbContext.SaveChanges();
+            return Ok(author);
+        }
+
+
+    }
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vezbi.Data;
+using Vezbi.Dtos;
+using Vezbi.Entities;
+
+namespace Vezbi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BooksDbContext dbContext;
+
+        public BookController(BooksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var result = dbContext.Books.Select(x => new BookDto
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                Price = x.Price,
+            }).ToList();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = dbContext.Authors.AsNoTracking().FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook(CreateBookDto dto)
+        {
+            var book = new Book
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Price = dto.Price,
+            };
+            dbContext.Books.Add(book);
+            dbContext.SaveChanges();
+            return Ok(book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book book)
+        {
+            var toUpdate = dbContext.Books.FirstOrDefault(x => x.Id == id);
+            if (toUpdate == null)
+            {
+                return NotFound();
+            }
+            toUpdate.Title = book.Title;
+            toUpdate.Description = book.Description;
+            toUpdate.Price = book.Price;
+            dbContext.SaveChanges();
+            return Ok(book);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = dbContext.Books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            dbContext.Books.Remove(book);
+            dbContext.SaveChanges();
+            return Ok(book);
+        }
+    }
+}
